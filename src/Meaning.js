de5e3e50cd21a99bbcf985df73837c143edb3fd7
delaneytiger21 <89812496+delaneytiger21@users.noreply.github.com>
2022-08-22 16:55:44 -0500

    Add Phonetics

--- src/Meaning.js
+++ src/Meaning.js
@@ -3,20 +3,15 @@ import Synonyms from "./Synonyms";
 import "./Meaning.css"
 
 export default function Meaning(props) {
-  console.log(props);
   return (
     <div className="Meaning">
       <h6>{props.meaning.partOfSpeech}</h6>
       {props.meaning.definitions.map(function (info, index) {
         return (
           <div key={index}>
-            <p>
-              <strong>Definition:</strong> {info.definition}
-              <br />
-              <strong>Example:</strong> {info.example}
-              <br />
-              <Synonyms synonyms={info.synonyms} />
-            </p>
+            <p>Definition: {info.definition}</p>
+            <p>Example: {info.example}</p>
+            Synonyms: <Synonyms synonyms={info.synonyms} />
           </div>
         );
       })}
--- src/Results.js
+++ src/Results.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetics from "./Phonetics";
 import "./Results.css";
 
 export default function Results(props) {
@@ -7,6 +8,13 @@ export default function Results(props) {
     return (
       <div className="Results m-5">
         <h2>{props.results.word}</h2>
+        {props.results.phonetics.map(function (phonetics, index) {
+          return (
+            <div key={index}>
+              <Phonetics phonetics={phonetics} />
+            </div>
+          );
+        })}
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
