74829aa5baededecf60b66ce6f9562ef8bea6567
delaneytiger21 <89812496+delaneytiger21@users.noreply.github.com>
2022-08-30 19:49:49 -0500

    Add photo api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,20 +1,31 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
-    setResults(response.data[0])
+    setResults(response.data[0]);
+  }
+
+  function handlePexelResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelKey = "563492ad6f917000010000012a94056870a1435a84269bfb1cf07258";
+    let pexelUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=15`;
+    let headers = { "Authorization": `Bearer ${pexelKey}` };
+    axios.get(pexelUrl, { headers: headers }).then(handlePexelResponse);
   }
 
   function handleSubmit(event) {
@@ -35,13 +46,14 @@ export default function Dictionary(props) {
     return (
       <div className="Dictionary">
         <section>
-          <h1 className="m-4">What word do you want to look up?</h1>
+          <h1 className="m-3 text-center">What word do you want to look up?</h1>
           <form onSubmit={handleSubmit}>
             <input type="search" onChange={handleKeyword} defaultValue={props.defaultKeyword} />
           </form>
           <div className="hint m-1">Suggested words: wine, cat, beach, volleyball</div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
--- src/Phonetics.js
+++ src/Phonetics.js
@@ -1,10 +1,14 @@
 import React from "react";
 
 export default function Phonetics(props) {
-  console.log(props.phonetics);
-  return (
-    <div className="Phonetics text-center">
-      {props.phonetics.text}
-    </div>
-  );
+  if (props.phonetics) {
+    return (
+      <div className="Phonetics text-center">
+        {props.phonetics.text}
+      </div>
+    );
+  } else {
+    return null;
+  }
+
 }
